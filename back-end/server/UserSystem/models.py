da08c2d453dd3ec23189296caf0e1745869bb6c4
JianyuTANG <jianyutang99@gmail.com>
2019-10-11 13:43:17 +0800

    后端登录接口及基本用户数据库

--- back-end/server/UserSystem/models.py
+++ back-end/server/UserSystem/models.py
@@ -3,10 +3,13 @@ from django.utils import timezone
 
 # Create your models here.
 class User(models.Model):
-    username = models.CharField(max_length=32, unique=True)
-    openid = models.CharField(max_length=32, unique=True)  # 用户的唯一标识，微信官方提供
-    logon_status = models.CharField(max_length=32, unique=True)  # 用户是否已注册，详见竞品分析
-    password = models.CharField(max_length=32)  # 不考虑后期扩展，只限于微信小程序，则无需密码
+    username = models.CharField(max_length=32, unique=True)  # 用户名（或者叫昵称？）此为可选项，留待讨论
+    openid = models.CharField(max_length=32, unique=True)    # 用户的唯一标识，微信官方提供
+    logon_status = models.CharField(max_length=32)  # 用户身份的验证状态，guest or alumni
+    real_name = models.CharField(max_length=32)     # 真名
+    number_of_entry = models.IntegerField()         # 期数
+    student_id = models.CharField(max_length=32)    # 学号
+    password = models.CharField(max_length=32)      # 后期扩展用。若只限于微信小程序，则无需密码
     create_time = models.DateTimeField(default=timezone.now())
 
     def __str__(self):
