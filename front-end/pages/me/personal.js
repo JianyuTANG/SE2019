df8da66cd89a9a1424a670010f1a1d1b0b78ecfc
chuchong <33688310+chuchong@users.noreply.github.com>
2019-11-17 02:27:14 +0800

    制作了与后端通讯的函数
    1. 获取用户信息
    2. 修改用户信息
    3. 相比接口内容,少了wechatId , hobby, career三个内容, 通过''糊弄过去

--- front-end/pages/me/personal.js
+++ front-end/pages/me/personal.js
@@ -1,8 +1,13 @@
 // eslint-disable-next-line camelcase
 import { areaList } from '../../data/area.js'// 城市的表
 import { fieldList } from '../../data/field.js'// 工作领域的表
+const app = getApp()
+const sessionCode = '123456' // 假如我们有一个sessionCode
 Page({
   data: {
+    baseUrl: app.globalData.baseUrl,
+    getInfoUrl: app.globalData.baseUrl + 'view_user',
+    changeInfoUrl: app.globalData.baseUrl + 'modify_user_info',
     showTopTips: false,
     areaList: areaList,
     areaPlaceholder: ['请选择', '请选择', '请选择'],
@@ -40,7 +45,67 @@ Page({
   },
   onLoad () {
     console.log(this.data.areaList)
+    this.getInfo()
   },
+  // ---------------------------------------以下是和后端联系的函数--------------------------
+  getInfo () {
+    // 获取用户的个人信息,填写表格
+    let that = this
+    wx.request({
+      url: that.data.getInfoUrl,
+      data: {
+        sessionCode: sessionCode
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success (res) {
+        console.log(res)
+        console.log(res.data)
+        this.setData({
+          'basicData.name': res.data.name,
+          'basicData.num': res.data.num,
+          'basicData.identity': res.data.identity,
+          'basicData.department': res.data.department,
+          'formData.cityCode': res.data.city,
+          'formData.fieldCode': res.data.field,
+          'formData.telephone': res.data.tel,
+          'formData.email': res.data.email
+        })
+      }
+    })
+  },
+  changeInfo () {
+    // 根据用户填些信息,更新用户个人信息
+    let that = this
+    let formData = that.data.formData
+    let basicData = that.data.basicData
+    wx.request({
+      url: that.data.changeInfoUrl, // 仅为示例，并非真实的接口地址
+      data: {
+        sessionCode: sessionCode,
+        tel: formData.telephone,
+        email: formData.email,
+        selfDiscription: formData.content,
+        city: formData.cityCode,
+        field: formData.fieldCode,
+        wechatId: '',
+        company: '',
+        hobby: ''
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success (res) {
+        console.log(res.data)
+        that.successSubmit()
+      },
+      fail (res) {
+        that.failSubmit()
+      }
+    })
+  },
+  // ----------------------------------------------------------------------------------------
   formAreaChange (e) {
     const { field } = e.currentTarget.dataset // es6 语法糖, const {a} = b === const a = b.a
     let tar = e.detail.values[1]
@@ -77,12 +142,22 @@ Page({
           })
         }
       } else {
-        wx.showToast({
-          title: '修改成功',
-          duration: 500
-        })
-        setTimeout(wx.navigateBack, 500)
+        this.changeInfo()
       }
+    })
+  },
+  successSubmit: function () {
+    wx.showToast({
+      title: '修改成功',
+      duration: 1000
+    })
+    setTimeout(wx.navigateBack, 1000)
+  },
+  failSubmit: function () {
+    wx.showToast({
+      icon: 'none',
+      title: '连接服务器失败!',
+      duration: 1000
     })
   },
   submitCancel: function () {
