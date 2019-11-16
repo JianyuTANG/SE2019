700f0a85b8dd15e1629002547345e5ffc5e8dd5d
chuchong <33688310+chuchong@users.noreply.github.com>
2019-11-17 01:48:36 +0800

    做好了个人界面前端展示的部分
    1. 添加了必填项等规则,规则不成立时返回错误提示
    2. 添加了将输入绑定到逻辑层的函数,其中Input是用bindInput, textArea是bindblur(失去焦点),vant-area是bindChange
    3.添加了成功返回前的提示信息,500ms

--- front-end/pages/me/personal.js
+++ front-end/pages/me/personal.js
@@ -1,97 +1,70 @@
 // eslint-disable-next-line camelcase
-import { areaList } from '../../data/area.js'
-import { fieldList } from '../../data/field.js'
+import { areaList } from '../../data/area.js'// 城市的表
+import { fieldList } from '../../data/field.js'// 工作领域的表
 Page({
   data: {
     showTopTips: false,
     areaList: areaList,
     areaPlaceholder: ['请选择', '请选择', '请选择'],
     fieldList: fieldList,
-    radioItems: [
-      { name: 'cell standard', value: '0', checked: true },
-      { name: 'cell standard', value: '1' }
-    ],
-    checkboxItems: [
-      { name: 'standard is dealt for u.', value: '0', checked: true },
-      { name: 'standard is dealicient for u.', value: '1' }
-    ],
-    basicData: {
+    basicData: { // 读取的用户的基本数据
       name: '',
       num: '',
       identity: '',
       department: ''
-    },
+    }, // 可以被修改的提交表单的数据
     formData: {
-      title: '',
-      contact: '',
       telephone: '',
       email: '',
-      qualification: '',
+      cityCode: '',
+      fieldCode: '',
       content: ''
     },
-    rules: [{
-      name: 'title',
-      rules: { required: true, message: '单选列表是必选项' }
-    }, {
-      name: 'contact',
-      rules: { required: true, message: '多选列表是必选项' }
-    }, {
-      name: 'telephone',
-      rules: { required: true, message: 'qq必填' }
-    }, {
-      name: 'email',
-      rules: { required: true, message: '验证码必填' }
-    }, {
-      name: 'qualification',
-      rules: { required: true, message: 'idcard必填' }
-    }, {
-      name: 'content',
-      rules: { required: true, message: 'idcard必填' }
-    }],
-    files: [{
-      url: 'http://mmbiz.qpic.cn/mmbiz_png/VUIF3v9blLsicfV8ysC76e9fZzWgy8YJ2bQO58p43Lib8ncGXmuyibLY7O3hia8sWv25KCibQb7MbJW3Q7xibNzfRN7A/0'
-    }],
-    tapButtonDate: false
+    // 规则中暂时只有手机号码,email, 城市, 和 行业领域必填
+    rules: [
+      {
+        name: 'telephone',
+        rules: [{ required: true, message: '电话必填' }, { mobile: true, message: '电话应该有正确格式' }]
+      },
+      {
+        name: 'email',
+        rules: [{ required: true, message: '邮箱必填' }, { email: true, message: '邮箱应该有正确格式' }]
+      },
+      {
+        name: 'cityCode',
+        rules: { required: true, message: '城市是必填项' }
+      }, {
+        name: 'fieldCode',
+        rules: { required: true, message: '行业领域是必填项' }
+      }]
   },
   onLoad () {
-    this.setData({
-      selectFile: this.selectFile.bind(this),
-      uplaodFile: this.uplaodFile.bind(this)
-    })
     console.log(this.data.areaList)
   },
-  radioChange: function (e) {
-    console.log('radio发生change事件，携带value值为：', e.detail.value)
-
-    var radioItems = this.data.radioItems
-    for (var i = 0, len = radioItems.length; i < len; ++i) {
-      radioItems[i].checked = radioItems[i].value == e.detail.value
+  formAreaChange (e) {
+    const { field } = e.currentTarget.dataset // es6 语法糖, const {a} = b === const a = b.a
+    let tar = e.detail.values[1]
+    let value = ''
+    console.log(value)
+    if (typeof (tar) !== 'undefined' && tar.code !== '') {
+      value = tar.code
     }
-
+    console.log(value)
     this.setData({
-      radioItems: radioItems,
-      [`formData.radio`]: e.detail.value
+      [`formData.${field}`]: value
     })
   },
-  checkboxChange: function (e) {
-    console.log('checkbox发生change事件，携带value值为：', e.detail.value)
-
-    var checkboxItems = this.data.checkboxItems; var values = e.detail.value
-    for (var i = 0, lenI = checkboxItems.length; i < lenI; ++i) {
-      checkboxItems[i].checked = false
-
-      for (var j = 0, lenJ = values.length; j < lenJ; ++j) {
-        if (checkboxItems[i].value == values[j]) {
-          checkboxItems[i].checked = true
-          break
-        }
-      }
-    }
-
+  formInputChange (e) {
+    const { field } = e.currentTarget.dataset
     this.setData({
-      checkboxItems: checkboxItems,
-      [`formData.checkbox`]: e.detail.value
+      [`formData.${field}`]: e.detail.value
     })
+  },
+  formTextareaBlur (e) {
+    this.setData({
+      'formData.content': e.detail.value
+    })
+    console.log(this.data.formData.content)
   },
   submitForm: function (e) {
     this.selectComponent('#form').validate((valid, errors) => {
@@ -105,110 +78,14 @@ Page({
         }
       } else {
         wx.showToast({
-          title: '校验通过'
+          title: '修改成功',
+          duration: 500
         })
-        let id = 100
-        let resourceId = 'resource' + String(id)
-        let src = '/assets/activity.png'
-        let info = {
-          id: id,
-          title: this.data.formData.title,
-          content: this.data.formData.content,
-          contact: this.data.formData.contact,
-          telephone: this.data.formData.telephone,
-          email: this.data.formData.email,
-          qualification: this.data.formData.qualification,
-          startDate: this.data.date.currentDate,
-          endDate: this.data.date.endDate,
-          imageSrc: src
-        }
-        let fList = wx.getStorageSync('facultyList')
-        fList.push(info)
-        wx.setStorageSync('facultyList', fList)
-        wx.setStorageSync(resourceId, info)
-        wx.navigateBack()
+        setTimeout(wx.navigateBack, 500)
       }
     })
   },
   submitCancel: function () {
     wx.navigateBack()
-  },
-  chooseImage: function (e) {
-    var that = this
-    wx.chooseImage({
-      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-      success: function (res) {
-        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
-        that.setData({
-          files: that.data.files.concat(res.tempFilePaths)
-        })
-      }
-    })
-  },
-  previewImage: function (e) {
-    wx.previewImage({
-      current: e.currentTarget.id, // 当前显示图片的http链接
-      urls: this.data.files // 需要预览的图片http链接列表
-    })
-  },
-  selectFile: function (files) {
-    console.log('files', files)
-    // 返回false可以阻止某次文件上传
-  },
-  uplaodFile: function (files) {
-    console.log('upload files', files)
-    // 文件上传的函数，返回一个promise
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        // reject('some error')
-        resolve({ urls: ['127.0.0.1'] })
-      }, 1000)
-    })
-  },
-  uploadError: function (e) {
-    console.log('upload error', e.detail)
-  },
-  uploadSuccess: function (e) {
-    console.log('upload success', e.detail)
-  },
-  formatter: function (type, value) {
-    if (type === 'year') {
-      return `${value}年`
-    } else if (type === 'month') {
-      return `${value}月`
-    }
-    return value
-  },
-  tapButtonDate: function (e) {
-    this.setData({
-      'tapButtonDate': false
-    })
-  },
-  setDate: function (e) {
-    this.setData({
-      'tapButtonDate': true
-    })
-  },
-  changeEndDate: function (e) {
-    console.log(e)
-    let timeStamp = e.detail
-    let date = new Date(timeStamp).toDateString()
-    this.setData({
-      'date.endDay': date,
-      'date.endTimeStamp': timeStamp
-    })
-  },
-  onStatusChange: function (e) {
-    console.log(e)
-    const texts = e.detail.text
-    this.setData({
-      'formData.content': texts })
-  },
-  onEditorReady: function () {
-    const that = this
-    wx.createSelectorQuery().select('#editor').context(function (res) {
-      that.editorCtx = res.context
-    }).exec()
   }
 })
