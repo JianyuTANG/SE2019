7d3d822d8df7ae5423e32fe576df71c2d2f90af4
wang-sj16 <wang-sj16@mails.tsinghua.edu.cn>
2019-12-01 17:49:02 +0800

    feature: 增加query_res_interested

--- back-end/server/ResourceSystem/views.py
+++ back-end/server/ResourceSystem/views.py
@@ -278,6 +278,44 @@ def query_res_issued(request):
         res_list.append(tmp)
     return JsonResponse({"res_list": res_list})
     
+def query_res_interested(request):
+    '''
+    用于用户查看感兴趣的资源
+    :param request:
+    :return:
+    '''
+    post_body = request.body
+    json_request = json.loads(post_body)
+    session_code = json_request['sessionCode']
+    if session_code is None:
+        res = HttpResponse()
+        res.status_code = 404
+        return res
+#    if session_code != administration_config['session_code']:
+#        res = HttpResponse()
+#        res.status_code = 404
+#        return res
+    openid = json_request['openid']
+    resources = Resource.objects.filter()
+    res_list = []
+    for e in resources:
+        interest_list = e.interest_users.split(",")
+        if openid in interest_list:
+            tmp = {}
+            tmp['title'] = e.title
+            tmp['name'] = e.name
+            tmp['createTime'] = e.c_time
+            tmp['imgUrl'] = e.img_arr.split(",")
+            tmp['coverImg'] = e.cover_img
+            tmp['tags'] = e.tag_arr.split(",")
+            tmp['category'] = e.category
+            tmp['contact'] = e.contact
+            tmp['due'] = e.due
+            tmp['resID'] = e.res_id
+            tmp['interested'] = True
+            res_list.append(tmp)
+    return JsonResponse({"res_list": res_list})
+
 def switch_interest(request):
     '''
     用于用户切换是否喜爱
