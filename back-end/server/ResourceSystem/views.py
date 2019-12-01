28bae7065c1ba645326ef0920aaea0bb164b7de4
wang-sj16 <wang-sj16@mails.tsinghua.edu.cn>
2019-12-01 12:33:56 +0800

    feature: 增加res_modify

--- back-end/server/ResourceSystem/views.py
+++ back-end/server/ResourceSystem/views.py
@@ -94,7 +94,54 @@ def delete_res(request):
 
 
 def modify_res(request):
-    pass
+    '''
+    用于用户提交新的资源
+    :param request:
+    :return:
+    '''
+    post_body = request.body
+    json_request = json.loads(post_body)
+    session_code = json_request['sessionCode']
+    if session_code is None:
+        res = HttpResponse()
+        res.status_code = 404
+        return res
+    if session_code != administration_config['session_code']:
+        res = HttpResponse()
+        res.status_code = 404
+        return res
+    openid = json_request['openid']
+    res_id = json_request['resID']
+    try:
+        resource = Resource.objects.filter(res_id=res_id)[0]
+    except Exception as e:
+        print(e)
+        res = HttpResponse()
+        res.status_code = 404
+        return res
+    if not resource.openid == openid:
+        res = HttpResponse()
+        res.status_code = 404
+        print("error: user invalid!")
+        return res
+    title = json_request['title']
+    content = json_request['content']
+    due = json_request['due']
+    contact = json_request['contact']
+    img_arr = json_request['imgArr']
+    imgs = ''
+    for item in img_arr:
+        imgs = imgs + item + ','
+    imgs = imgs[:-1]
+    resource.title = title
+    resource.content = content
+    resource.due = due
+    resource.contact = contact
+    resource.img_arr = imgs
+    resource.save()
+    res = HttpResponse()
+    res.status_code = 200
+    return res
 
 def view_res(request):
     pass
