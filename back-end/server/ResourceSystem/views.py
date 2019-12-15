9b92263a68c190be918cb743af6d7c2c1ce567f3
wang-sj16 <wang-sj16@mails.tsinghua.edu.cn>
2019-12-15 14:22:28 +0800

    feature: 增加search功能V1.0

--- back-end/server/ResourceSystem/views.py
+++ back-end/server/ResourceSystem/views.py
@@ -323,6 +323,45 @@ def query_res_by_openid(request):
         res_list.append(tmp)
     return JsonResponse({"res_list": res_list})
 
+def serch_res(request):
+    '''
+    搜索资源
+    :param request:
+    :return:
+    '''
+    post_body = request.body
+    json_request = json.loads(post_body)
+    session_code = json_request['sessionCode']
+    if session_code is None:
+        res = HttpResponse()
+        res.status_code = 404
+        return res
+#    if session_code != administration_config['session_code']:
+#        res = HttpResponse()
+#        res.status_code = 404
+#        return res
+    openid = json_request['openid']
+    search_content = json_request['content'].strip()
+    resources = Resource.objects.filter()
+    res_list = []
+    for e in resources:
+        if search_content in e.title or search_content in e.contact:
+            interest_list = e.interest_users.split(",")
+            tmp = {}
+            tmp['title'] = e.title
+            tmp['name'] = e.name
+            tmp['createTime'] = e.c_time
+            tmp['imgUrl'] = e.img_arr.split(",")
+            tmp['coverImg'] = e.cover_img
+            tmp['tags'] = e.tag_arr.split(",")
+            tmp['category'] = e.category
+            tmp['contact'] = e.contact
+            tmp['due'] = e.due
+            tmp['resID'] = e.res_id
+            tmp['interested'] = openid in interest_list
+            res_list.append(tmp)
+    return JsonResponse({"res_list": res_list})
+
 def query_res_interested(request):
     '''
     用于用户查看感兴趣的资源
