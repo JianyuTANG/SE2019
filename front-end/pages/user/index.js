9abc60fc7056306485db5895a108b22158093efd
chuchong <33688310+chuchong@users.noreply.github.com>
2019-12-18 11:17:38 +0800

    fix 修改了和后端连接的接口, feature 添加了转换到资源的跳转,优化了页面显示
    This reverts commit 6685864e7eff010d331ea954cd7720953ff0318d.

--- front-end/pages/user/index.js
+++ front-end/pages/user/index.js
@@ -1,19 +1,23 @@
 /* 他人详情页面,传入options中的targetOpenid字段 */
+import { areaList } from '../../data/area.js'// 城市的表
+import { fieldList } from '../../data/field.js'// 工作领域的表
+import { departList } from '../../data/department.js'
 const app = getApp()
 Page({
   data: {
     baseUrl: app.globalData.baseUrl,
     baseUrlPrefix: app.globalData.baseUrl.substr(0, app.globalData.baseUrl.length - 1),
+    queryUrl: app.globalData.baseUrl + 'query_res_by_openid',
     viewUserUrl: app.globalData.baseUrl + 'view_other_by_openid',
     activeNames: ['1'],
     avatarUrl: '',
-    name: 'test',
-    email: 'abc',
+    name: '',
+    email: '',
     department: '',
     city: '',
     field: '',
-    telephone: 123,
-    content: '123',
+    telephone: '',
+    content: '',
     showResouceList: [],
     tarOpenid: '',
     stuNum: [],
@@ -36,26 +40,37 @@ Page({
       that.query_user(resolve, reject)
     })
 
-    promiseUser.then((res) => {
-      console.log('promise user' + res)
-      let resData = res.data
-      resData['avatar_url'] = that.data.baseUrlPrefix + resData['avatar_url']
-      let resList = res.data.resArr.filter(d => d).map(item => {
+    promiseRes.then((res) => {
+      console.log(res)
+      let resList = res.data.res_list.filter(d => d).map(item => {
         let x = item
         x['coverImg'] = that.data.baseUrlPrefix + item['coverImg']
         return x
       })
+      console.log(resList)
       that.setData({
+        showResouceList: resList
+      })
+    }).catch(res => { console.log(res) })
+
+    promiseUser.then((res) => {
+      console.log('promise user' + res)
+      let resData = res.data
+      resData['avatar_url'] = that.data.baseUrlPrefix + resData['avatar_url']
+      let city = areaList['city_list'][resData.city] || ''
+      let field = fieldList['city_list'][resData.field] || ''
+      let depart = departList[resData.department]
+      that.setData({
+        city: city,
         name: resData.name,
-        stuNum: resData.studentNum,
-        advNum: resData.advisorNum,
-        department: resData.department,
+        stuNum: resData.studentArr,
+        advNum: resData.advisorArr,
+        department: depart,
         email: resData.email,
-        field: resData.field,
+        field: field,
         telephone: resData.tel,
         content: resData.selfDiscription,
-        avatarUrl: resData['avatar_url'],
-        showResouceList: resList
+        avatarUrl: resData['avatar_url']
       })
     }).catch(res => { console.log(res) })
   },
@@ -91,6 +106,39 @@ Page({
     })
   },
 
+  query_res: function (resolve, reject) {
+    let that = this
+    let sessionCode = wx.getStorageSync('sessionCode')
+    let openid = wx.getStorageSync('openid')
+    let tOpenid = that.data.tarOpenid
+    let url = that.data.queryUrl
+    wx.request({
+      url: url,
+      method: 'POST',
+
+      data: {
+        sessionCode: sessionCode,
+        openid: openid,
+        targetOpenid: tOpenid
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success (res) {
+        console.log('query res返回值', res)
+        if (res.statusCode === 200) {
+          resolve(res)
+        } else {
+          reject(new Error('server rejects'))
+        }
+      // allList
+      },
+      fail: (res) => {
+        reject(new Error('server rejects'))
+      }
+    })
+  },
+
   onShareAppMessage: function () {
     return {
       title: '自定义分享标题',
@@ -102,6 +150,14 @@ Page({
   onChange (event) {
     this.setData({
       activeNames: event.detail
+    })
+  },
+
+  onReadmore: function (e) {
+    console.log('e.currentTarget:', e.currentTarget)
+    let resID = e.currentTarget.dataset.id
+    wx.navigateTo({
+      url: '/pages/resource/detail?resID=' + resID
     })
   }
 })
